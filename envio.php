d112adb02397ba9f922e659f9ccd05111c9af2c8
Eder <eder.xavier.ti@gmail.com>
2021-04-11 10:43:52 -0300

    corrigindo erro de edição

--- envio.php
+++ envio.php
@@ -2,7 +2,7 @@
 <body>
 <?php
     echo '<p>Enviando direto do VSCode do Windnows 10</p>';
-    echo '<br>'
+    echo '<br>';
     echo '<p>Editando o arquivo diretamente no Github</p>';
 ?>
 </body>
